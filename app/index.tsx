48bdb73bb90fb178ec4d640efe47d5d6cca78c69
SayanDasDev <mesayan19@gmail.com>
2024-04-12 22:39:45 +0530

    assets loaded

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,15 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
 
-const index = () => {
-  return (
-    <View className='flex-1 justify-center items-center'>
-      <Text>index</Text>
+export default function Ap() {
+  return(
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-3xl font-pblack">
+        Hello, World!
+      </Text>
+      <Link href={"/profile"} className="text-blue-400"> go to profile</Link>
+      <StatusBar style="auto" />
     </View>
   )
 }
-
-export default index
-
-const styles = StyleSheet.create({})
